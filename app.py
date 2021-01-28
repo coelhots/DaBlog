c730446f881f6fb3c72d3025f44dd2a047aebb6f
Daniel Coelho <danielcamiranda@hotmail.com>
2021-01-28 12:28:30 -0300

    new fix

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/coelho/Documents/Blog/blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
-
+######################################
 db = SQLAlchemy(app)
 
 class Blogpost(db.Model):
