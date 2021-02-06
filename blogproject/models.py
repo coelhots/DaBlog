316c4e74c54231f61ef527587b92ce5ab65db4a4
Daniel Coelho <danielcamiranda@hotmail.com>
2021-02-05 21:56:21 -0300

    added missing folders

---
+++ blogproject/models.py
@@ -0,0 +1,33 @@
+from blogproject import db,login_manager
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+class User(db.Model,UserMixin):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(64), unique=True,index=True)
+    username = db.Column(db.String(64),unique=True,index=True)
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self,email,username, password):
+        self.email = email
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+class Blogpost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    author = db.Column(db.String(20))
+    date_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
